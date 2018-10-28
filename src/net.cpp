4360ebe56ba98e7080673d5e9b3d7c7e5cf3ac88
isacoin <39123587+oia-isacoin@users.noreply.github.com>
2018-10-29 10:18:37 +1100

    Update net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -1063,10 +1063,14 @@ void ThreadMapPort()
 #ifndef UPNPDISCOVER_SUCCESS
     /* miniupnpc 1.5 */
     devlist = upnpDiscover(2000, multicastif, minissdpdpath, 0);
-#else
+#elif MINIUPNPC_API_VERSION < 14
     /* miniupnpc 1.6 */
     int error = 0;
     devlist = upnpDiscover(2000, multicastif, minissdpdpath, 0, 0, &error);
+#else
+    /* miniupnpc 1.9.20150730  */
+    int error = 0;
+    devlist = upnpDiscover(2000, multicastif, minissdpdpath, 0, 0, 2, &error);
 #endif
 
     struct UPNPUrls urls;
@@ -1093,7 +1097,7 @@ void ThreadMapPort()
             }
         }
 
-        string strDesc = "Isacoin " + FormatFullVersion();
+        string strDesc = "Omcoin " + FormatFullVersion();
 
         try {
             loop {
@@ -1173,12 +1177,12 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {"omisaal.com", "isaseed.omisaal.com"},
+    {"omisaal.com", "omseed.omisaal.com"},
     {NULL, NULL}
 };
 
 static const char *strTestNetDNSSeed[][2] = {
-    {"omisaal.com", "isaseed.omisaal.com"},
+    {"omisaal.com", "omseed.omisaal.com"},
     {NULL, NULL}
 };
 
@@ -1660,7 +1664,7 @@ bool BindListenPort(const CService &addrBind, string& strError)
     {
         int nErr = WSAGetLastError();
         if (nErr == WSAEADDRINUSE)
-            strError = strprintf(_("Unable to bind to %s on this computer. Isacoin is probably already running."), addrBind.ToString().c_str());
+            strError = strprintf(_("Unable to bind to %s on this computer. Omcoin is probably already running."), addrBind.ToString().c_str());
         else
             strError = strprintf(_("Unable to bind to %s on this computer (bind returned error %d, %s)"), addrBind.ToString().c_str(), nErr, strerror(nErr));
         printf("%s\n", strError.c_str());
